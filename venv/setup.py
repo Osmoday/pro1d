490635a610e26fa2b5fddbaeb21cca8ef12e1217
Osmoday <s16762@pjwstk.edu.pl>
2020-01-13 19:45:13 +0100

    setup

---
+++ venv/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pro1d',
+   version='1.0',
+   description='',
+   author='Tadeusz Pawlonka, Dmytro Shevchenko, Witold Kurpiewski',
+   author_email='',
+   packages=['pro1d'],
+   install_requires=['pandas', 'numpy']
+)
