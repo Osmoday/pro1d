3a4578d6caf6ddf6e13402ab31cfc6697c1e9dfb
Osmoday <s16762@pjwstk.edu.pl>
2020-01-14 22:12:33 +0100

    better file saving options

--- venv/pro1d/main.py
+++ venv/pro1d/main.py
@@ -54,20 +54,25 @@ print(association_rules_fpg)
 print("FP-growth association rule mining execution time: ", association_fpg_time, " seconds")
 print("")
 
-# df.to_csv('readout.csv')
 inp = input("Save results as .csv files? [Y/N]")
 if inp.lower() == "y" or inp.lower() == "yes":
-    filename_dataframe = input("Please name the file for the dataframe (without file format extension)")
-    df.to_csv(filename_dataframe+".csv")
-
-    filename_apriori = input("Please name the file for the apriori results (without file format extension)")
+    filename_apriori = input("Please name the file for the apriori results (without file format extension) ")
     frequent_itemsets.to_csv(filename_apriori + ".csv")
 
-    filename_a_association = input("Please name the file for the apriori association rules (without file format extension)")
+    filename_a_association = input("Please name the file for the apriori association rules (without file format "
+                                   "extension) ")
     association_rules_apriori.to_csv(filename_a_association + ".csv")
 
-    filename_fpg = input("Please name the file for the FP-growth results (without file format extension)")
+    filename_fpg = input("Please name the file for the FP-growth results (without file format extension) ")
     fpg.to_csv(filename_fpg + ".csv")
 
-    filename_dpg_association = input("Please name the file for the FP=growth association rules (without file format extension)")
+    filename_dpg_association = input("Please name the file for the FP=growth association rules (without file format "
+                                     "extension) ")
     association_rules_fpg.to_csv(filename_dpg_association + ".csv")
+
+inp = input("Save encoded dataframe as a .csv file? [Y/N] WARNING: Large file size measured in gigabytes, saving it "
+            "will take a very long time. ")
+if inp.lower() == "y" or inp.lower() == "yes":
+    filename_dataframe = input("Please name the file for the dataframe (without file format extension)")
+    df.to_csv(filename_dataframe + ".csv")
+
