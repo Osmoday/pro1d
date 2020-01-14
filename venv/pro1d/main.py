2c87a7c5a658df82045b5be94e2a154180a2366a
Osmoday <s16762@pjwstk.edu.pl>
2020-01-14 15:51:50 +0100

    added association rule mining

--- venv/pro1d/main.py
+++ venv/pro1d/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 with open("../data/retail.dat.txt", "r") as f:
     data = f.readlines()
@@ -17,3 +18,7 @@ frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(
 # frequent_itemsets = frequent_itemsets.drop()
 
 print(frequent_itemsets)
+
+print(association_rules(frequent_itemsets, metric="confidence", min_threshold=0.05))
+
+# df.to_csv('readout.csv')
